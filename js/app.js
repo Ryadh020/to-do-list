723b355ac03f8e8aecd868942ab33c264a357572
riyadh <riyadh02220@gmail.com>
2019-03-31 15:20:55 +0200

    now you can check or delete your to-do's

--- js/app.js
+++ js/app.js
@@ -11,13 +11,19 @@ const remove = document.querySelector(".fa-trash-alt");
 let LIST = [];
 let id = 0;
 
+// CLASSES NAMES ///////////////////////////////////////////////////////////////
+const ITS_DONE = "fa-bell-slash";
+const ALERT_ME = "fa-bell ";
+const LINE_THROUGH = "line";
+const GREEN = "green"
+
 // ADD A TO-DO ON CLICKING ENTER KEY ///////////////////////////////////////////
 input.addEventListener("keyup", (ev)=> {
   const key = ev.keyCode;
   const TODO = input.value;
   if(key === 13) {
     if(TODO) {
-      addToDo(TODO,id);
+      addToDo(TODO,id,false,false);
       LIST.push({
         id:id,
         done:false,
@@ -30,18 +36,48 @@ input.addEventListener("keyup", (ev)=> {
 })
 
 // ADD AN TO DO FUNCTION ////////////////////////////////////////////////////////
- function addToDo (TODO,id) {
-  const position = "beforeend";
+ function addToDo (TODO,id,done,trash) {
+    if(trash){return;}
+    const DONE = done?ITS_DONE:ALERT_ME;
+    const LINE = done?LINE_THROUGH:"";
+
    const toDo = `<li class="todoBar" id="${id}">
-                    <h1 class="todo" id="todo">${TODO}</h1>
-                    <i class="fas fa-trash-alt"></i>
+                    <h1 class="todo ${LINE}" id="todo">${TODO}</h1>
+                    <i class="fas fa-trash-alt " job="delet"></i>
                     <div class="separate"></div>
-                    <i class="fas fa-bell"></i>
+                    <i class="done fas ${DONE}" job="check"></i>
                 </li> `;
-  /*item.style.marginTop = "14px";
-  item.style.top = "0";*/
+  const position = "beforeend";
   main.insertAdjacentHTML(position,toDo);
+  }
+
+
+// check or delete ///////////////////////////////////////////////////////////////////
+
+main.addEventListener("click",(ev)=> {
+  const element = ev.target;
   
+  const elementJob = element.attributes.job.value;
+ 
+  if(elementJob == "check" ) {
+    chekIt(element)
+  }
+  if(elementJob == "delet") {
+    deleteIT(element)
   }
   
+})
+
+// check a to do ////////////////////////////////////////////////////////////////////
+function chekIt(element) {
+  element.classList.toggle(ITS_DONE);
+  element.style.fontSize ="30px"
+  element.parentNode.classList.toggle(GREEN);
+  element.parentNode.querySelector(".todo").classList.toggle(LINE_THROUGH);
   
+}
+
+// delete a todo //////////////////////////////////////////////////////////////////
+function deleteIT(element) {
+  element.parentNode.parentNode.removeChild(element.parentNode);
+}
